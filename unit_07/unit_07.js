2a2c9ace9cbc07f28b5b1840453a344f8481ed00
RackAnatoly <rack.anatoly@gmail.com>
2021-09-30 10:58:11 +0200

    1

--- unit_07/unit_07.js
+++ unit_07/unit_07.js
@@ -4,7 +4,8 @@
 let a1 = 8;
 
 function t1() {
-
+let out = document.querySelector('.out-1');
+out.innerHTML = a1;
 }
 
 document.querySelector('.b-1').onclick = t1;
@@ -16,7 +17,7 @@ document.querySelector('.b-1').onclick = t1;
 
 let a2 = 8;
 function t2() {
-
+return a2;
     //return
 }
 
@@ -32,7 +33,7 @@ document.querySelector('.b-2').onclick = function () {
 // Наша предыдущая функция, сильно все еще зависима от внешних переменных. Давайте сделаем ее более универсальной. Пусть функция t3 принимает 2 аргумента и возвращает (return) их произведение. Допишите код функции так, чтобы она возвращала произведение двух чисел, переданных ей в качестве аргументов a, b. Протестируем функцию на двух примерах, с помощью кнопок .b-3-1 и .b-3-2.
 
 function t3(a, b) {
-
+return a*b;
 }
 
 document.querySelector('.b-3-1').onclick = function () {
@@ -47,11 +48,11 @@ document.querySelector('.b-3-2').onclick = function () {
 // Напишите функцию t4 которая принимает ваш год рождения  и вычисляет (возвращает) ваш возраст. 
 
 function t4(year) {
-
+return 2021-year;
 }
 
 document.querySelector('.b-4').onclick = function () {
-    document.querySelector('.out-4').textContent = t4(1983);
+    document.querySelector('.out-4').textContent = t4(1989);
 }
 
 
@@ -60,11 +61,11 @@ document.querySelector('.b-4').onclick = function () {
 
 
 function t5(yourName) {
-
+return yourName.length;
 }
 
 document.querySelector('.b-5').onclick = function () {
-    document.querySelector('.out-5').textContent = t5('Alex');
+    document.querySelector('.out-5').textContent = t5('Anatoly');
 }
 
 
@@ -73,7 +74,7 @@ document.querySelector('.b-5').onclick = function () {
 // Напишите функцию t6, которая принимает 2 числа и возвращает случайное целое число от первого до второго принятого параметра. Внимание, это первая задача, решение которой нужно найти в google. Не пишите решение сами!
 
 function t6(a, b) {
-
+return Math.floor(Math.random() * (b - a)) + a;
 }
 
 document.querySelector('.b-6').onclick = function () {
@@ -92,9 +93,9 @@ function t7() {
     // у вас есть функция t6, которая может генерировать случайные целые числа в нужном диапазоне. 
     // давайте воспользуемся ней. Просто запустим ее три раза внутри строки ниже
     //return `rgb(${t6(0,255)}....допишите сами
-}
+    return `rgb(${Math.random()*256<<0}, ${Math.random()*256<<0}, ${Math.random()*256<<0})`;
 
-document.querySelector('.b-7').onclick = function () {
+    document.querySelector('.b-7').onclick = function () {
     document.querySelector('.out-7').style.background = t7();
 }
 
