334689dba538ac68b11ea32cb3383593feac0f15
Rak_Anatoli <rack.anatoy@gmail.com>
2021-10-25 10:39:11 +0200

    '

--- unit_10/unit_10.js
+++ unit_10/unit_10.js
@@ -45,9 +45,14 @@ document.querySelector('.b-3').onclick = f3;
 // Вывод в out-4
 // Тест допустим массив [1,2,3,4,5,6,7,8,9,10] вывод: 1 4 9
 
-// let ar4 =  // переменную обьявляем эту здесь!!!!
+let ar4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] // переменную обьявляем эту здесь!!!!
 
 function f4() {
+    for (i = 0; i < ar4.length; i++) {
+        if (ar4[0] && ar4[3] && ar4[8]) {
+            document.querySelector('.out-4').innerHTML = ar4[0];
+        }
+    }
 }
 
 document.querySelector('.b-4').onclick = f4;
