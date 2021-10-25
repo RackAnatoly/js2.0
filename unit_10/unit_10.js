ae86e4a0e3db9db4cb6a1579cd5b97be4c4e2854
Rak_Anatoli <rack.anatoy@gmail.com>
2021-10-25 20:51:47 +0200

    1

--- unit_10/unit_10.js
+++ unit_10/unit_10.js
@@ -168,6 +168,16 @@ document.querySelector('.b-10').onclick = f10;
 let ar11 = [2, 3, 4, 5, 6, 7];
 
 function f11() {
+    let a = ar11[2];
+    ar11[2] = ar11[4];
+    ar11[4] = a;
+
+    let d = '';
+
+    for (i = 0; i < ar11.length; i++) {
+        d += ar11[i] + ' ';
+    }
+    document.querySelector('.out-11').innerHTML = d;
 
 }
 
