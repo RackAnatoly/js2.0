d78bb68c8d847c8950db5916f75ea1f5d857900d
Rak_Anatoli <rack.anatoy@gmail.com>
2021-10-27 21:05:35 +0200

    7

--- unit_11/unit_11.js
+++ unit_11/unit_11.js
@@ -200,7 +200,11 @@ document.querySelector('.b-12').onclick = f12;
 let d13 = [6, 0, 22, 1, 4, 76];
 
 function f13() {
-
+    let b = [];
+    for (let i = d13.length - 1; i >= 0; i--) {
+        b.push(d13[i]);
+    }
+    d13 = b;
     showArr('.out-13', d13);
 }
 
