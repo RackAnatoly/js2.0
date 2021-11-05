e329b150fa09376c51398e99abfb90d6db719501
RackAnatoly <82027973+RackAnatoly@users.noreply.github.com>
2021-11-05 09:27:26 +0100

    Update unit_16.js
    task 9

--- unit_16/unit_16.js
+++ unit_16/unit_16.js
@@ -134,7 +134,13 @@ document.querySelector('.b-8').addEventListener('click', () => {
 let a9 = [[4, 3, 2], [2, 5], [0, 0, 0, 0, 0]];
 
 function f9() {
-
+let a = 0;
+    for (let i = 0; i < a9.length; i++) {
+        if (a9[i].length > a) {
+            a = a9[i].length - 1;
+        }
+    }
+    return a;
 }
 
 document.querySelector('.b-9').addEventListener('click', () => {
