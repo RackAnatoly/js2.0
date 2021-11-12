1e9cd65bcfa8de4af071d361284691d5ef659b93
RackAnatoly <82027973+RackAnatoly@users.noreply.github.com>
2021-11-12 21:51:06 +0100

    Update unit_19.js
    task 13

--- unit_19/unit_19.js
+++ unit_19/unit_19.js
@@ -151,13 +151,12 @@ document.querySelector('.div-12').onmousedown = () => {
 // Task 13 ============================================
 /*  Дан блок .div-13. Добавьте на него событие mousedown - при нажатии кнопки мыши - добавляйте ему класс active. Добавьте ему событие mouseup - при отпускании мыши - удаляйте класс active. */
 
-// () =>  {
-
-// }
-
-// () =>  {
-
-// }
+document.querySelector('.div-13').onmousedown = () => {
+    document.querySelector('.div-13').classList.add('active');
+};
+document.querySelector('.div-13').onmouseup = () => {
+    document.querySelector('.div-13').classList.remove('active');
+};
 // ваше событие здесь!!!
 
 
