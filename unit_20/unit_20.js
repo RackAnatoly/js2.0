50a1cad1556dfeb9297059d0355fa2e6754019ab
RackAnatoly <82027973+RackAnatoly@users.noreply.github.com>
2021-11-15 21:17:44 +0100

    Update unit_20.js
    task 2

--- unit_20/unit_20.js
+++ unit_20/unit_20.js
@@ -14,7 +14,7 @@ document.querySelector('.i-1').onkeydown = t1;
 
 function t2(event) {
     console.log(event);
-
+ document.querySelector('.out-2').innerHTML = event.keyCode;
 }
 
 document.querySelector('.i-2').onkeypress = t2;
